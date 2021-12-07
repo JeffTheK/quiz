0723430404b432df3d1e43f6f1514bd4abc59b5c
JeffTheK <jeff144@protonmail.com>
2021-12-08 00:42:33 +0200

    Remove old run test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,5 @@
 import quiz.main
 
-def test_main(capsys):
-    quiz.main.run()
-    out, err = capsys.readouterr()
-    assert(out == "hello world\n")
-
 def test_load_questions():
     out = quiz.main.load_questions()
     assert(len(out) == 2)
